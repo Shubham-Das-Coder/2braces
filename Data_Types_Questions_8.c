388633e384d48d2aad7041f284592d35f5860d20
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-07-14 09:34:51 +0530

    Add files via upload

---
+++ Data_Types_Questions_8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float me = 5.25;
+    double you = 5.25;
+    if (me == you)
+        printf("I love U");
+    else
+        printf("I hate U");
+    return 0;
+}
